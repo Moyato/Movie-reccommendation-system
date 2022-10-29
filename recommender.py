28228addd3cb04e35c2818c2e7383f4b3a8a23e7
Ayomide100 <fasakinoluwaseyi4@gmail.com>
2022-10-29 22:08:15 +0100

    Final version

---
+++ recommender.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import time
+import streamlit as st
+
+url="https://raw.githubusercontent.com/alooperalta/movieRecommendation/master/content/ml-latest-small/movies.csv"
+movies=pd.read_csv(url)
+movies.head()
+
+st.title('Movie Recommendation System')
+
+movie_title=movies['title']
+Genres = movies['genres'].str.get_dummies(sep='|')
+movies = pd.concat([movies, Genres], axis=1)
+movies.drop(['genres','(no genres listed)'],axis=1,inplace=True)
+movies.head()
+
+# Load the saved model
+import pickle
+filename = 'recommender.pkl'
+pickle_in = open(filename, 'rb')
+recommender= pickle.load(pickle_in)
+recommender.fit_predict(X)
+
+rndi=dict()
+for i in range(n_clusters):
+    rndi[i]=[]
+for i in range(len(labels)):
+    rndi[labels[i]].append(movie_title[i])
+
+
+def getRecom(x):
+    if(x!="Select an Option"):
+        ind=movie_title[movie_title==x].index[0]
+        choice=random.sample(rndi[labels[ind]],5)
+        st.write('\nFinding Recommendations Similar to', x,'...')
+        time.sleep(2)
+        st.write('\nTop Matches for',x, 'are:\n' )
+        for i in choice:
+            if (i==x):
+                st.write(random.sample(rndi[labels[ind]],1)[0]) 
+            else:
+                st.write(i)
+        st.write("")
+
+def model():        
+        o = movie_title
+        option = st.selectbox('Select your favourite movie',o)
+        getRecom(option)
+
+
+model()
+
+
+
